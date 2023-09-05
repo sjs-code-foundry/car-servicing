478254eeab2139d6b3e0ac1e61494cb863794d1d
sjs-code-foundry <bds.stephen@gmail.com>
2023-09-05 19:44:33 +0100

    2023/09/05 19:45

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app-check.js";
 import { getDatabase, ref, push, onValue, remove, query, orderByKey, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 import { connectDatabaseEmulator } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
@@ -15,6 +16,10 @@ const appSettings = {
     databaseURL: "https://playground-62567-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 const app = initializeApp(appSettings)
+const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider('6LfIDQAoAAAAAH9H3cK6a7tOtXZUK_b5So1t57LV'),
+    isTokenAutoRefreshEnabled: true
+})
 const database = getDatabase(app)
 
 
