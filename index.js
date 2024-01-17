0440a34ffd6d2810651f4c588c16416430756362
sjs-code-foundry <bds.stephen@gmail.com>
2024-01-17 17:52:51 +0000

    chore: Sort imports for clarity
    Put each import onto an individual line to make additions/subtractions easier

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 /* === Imports === */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js"
-import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app-check.js";
-import { getDatabase, ref, push, onValue, remove, query, orderByKey, get } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"
+import { initializeAppCheck,
+         ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app-check.js";
+import { getDatabase,
+         ref,
+         push,
+         onValue,
+         remove,
+         query,
+         orderByKey,
+         get } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"
 import { connectDatabaseEmulator } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"
 
 /* === Initialize Firebase === */
